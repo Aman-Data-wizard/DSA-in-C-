5ef3d215d7b811727b043b0ef74bc8cdfd1d8c8f
Aman <singh.mie4166@gmail.com>
2025-07-16 23:09:07 +0530

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+     int marks[5] = {99,100,50,60,68};
+     // marks[0] = 101; 
+     // cout<< marks[0] << endl;
+     // cout<< marks[1] << endl;
+     // cout<< marks[2] << endl;
+     // cout<< marks[3] << endl;
+     // cout<< marks[4] << endl;
+     int size = 5;
+
+     for(int i=0; i<size; i++){
+          cout << marks[i] << endl;
+     }
+     return 0;
+
+     double price[] = {99.99, 205.56, 30.00}; 
+}
